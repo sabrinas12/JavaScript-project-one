7d6da9678d291512bbcc8d9742689d9016daa27a
Sabrina Silva <111471279+sabrinas12@users.noreply.github.com>
2022-10-11 11:35:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 const calculadoraFolhaPagamento = {
     inss: function(valorPagamento) {
-        if (valorPagamento === undefined) {
-            return 'Por favor, insira o valor'; 
-        } else if (valorPagamento <= 1212) {
+        let resultado = Number(valorPagamento);
+        if (Number.isNaN(resultado)) {
+            throw new Error('Por favor, digite um valor');
+        } else {
+        } if (valorPagamento <= 1212) {
             return `O valor de contribuição do INSS é ${valorPagamento * 0.075}`;
         } else if (valorPagamento <= 2427.35) {
             return `O valor de contribuição do INSS é ${valorPagamento * 0.09}`; 
         } else if (valorPagamento <= 3641.03) {
-        return `O valor de contribuição do INSS é ${valorPagamento * 0.12}`;
+            return `O valor de contribuição do INSS é ${valorPagamento * 0.12}`;
         } else {
-        return `O valor de contribuição do INSS é ${valorPagamento * 0.14}`;
+            return `O valor de contribuição do INSS é ${valorPagamento * 0.14}`;
         }
     },
     falta: function(valorPagamento) {
-        if (valorPagamento === undefined) {
-            return 'Por favor, insira o valor';
+        let resultado = Number(valorPagamento);
+        if (Number.isNaN(resultado)) {
+            throw new Error('Por favor, digite um valor');
         } else {
             return `O valor da falta diária de um salário de ${valorPagamento} é ${valorPagamento / 30}`;  
         }
     },
     irrf: function(valorPagamento) {
-        if (valorPagamento === undefined) {
-            return 'Por favor, insira o valor';
+        let resultado = Number(valorPagamento);
+        if (Number.isNaN(resultado)) {
+            throw new Error('Por favor, digite um valor');
         } else {
         if (valorPagamento <= 1903.98) {
             return 'Isento de pagamento. Salário abaixo do valor de tabela.';
@@ -37,15 +41,17 @@ const calculadoraFolhaPagamento = {
         }
     },
     fgts: function(valorPagamento) {
-        if (valorPagamento === undefined) {
-            return 'Por favor, insira o valor';
+        let resultado = Number(valorPagamento);
+        if (Number.isNaN(resultado)) {
+            throw new Error('Por favor, digite um valor');
         } else {
             return `O valor de pagamento do FGTS é ${valorPagamento * 0.08}`;  
         }
     },
     horaExtra: function(valorPagamento, h50, h100) {
-        if (valorPagamento === undefined || h50 === undefined || h100 === undefined) {
-            return 'Por favor, insira as informações corretamente';
+        let resultado = Number(valorPagamento);
+        if (Number.isNaN(resultado)) {
+            throw new Error('Por favor, digite um valor');
         } else if (h50 == true && h100 == false) {
             return `O valor da hora extra de 50% é de ${(valorPagamento/220) * 0.5}`;
         } else if (h50 == false && h100 == true) {
